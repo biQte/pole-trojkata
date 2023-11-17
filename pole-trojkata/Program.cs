bcadb8ba483a7aeec4e6daa51f31d1bc196d3d99
biQte <patrykgonet@gmail.com>
2023-11-17 12:38:03 +0100

    Add project files.

---
+++ pole-trojkata/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pole_trojkata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczenie pola trójkąta");
+            Console.WriteLine("Podaj długość podstawy:");
+            float baseLength = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wysokość trójkąta:");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pole trójkąta o podanych wymiarach wynosi: " + ((baseLength * height) / 2) + " jednostek kwadratowych");
+        }
+    }
+}
